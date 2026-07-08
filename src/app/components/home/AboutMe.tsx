47dd76ca69064b99872fe34583f8cb89ac0e12b2
Navneet Patidar <navneetpatidar422@gmail.com>
2026-07-09 00:14:50 +0530

    s

--- src/app/components/home/AboutMe.tsx
+++ src/app/components/home/AboutMe.tsx
@@ -14,7 +14,7 @@ import { Button } from "../ui/button";
 
 const experiences = [
   {
-    role: "Designer",
+    role: "Graphic Designer",
     company: "InstantAI",
     period: "Apr 2026 – Present"
   },
