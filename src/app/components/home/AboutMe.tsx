b94af625139020da41124b53ae9fd5f71e1509b1
Navneet Patidar <navneetpatidar422@gmail.com>
2026-07-08 13:18:54 +0530

    new and final image for abt me

--- src/app/components/home/AboutMe.tsx
+++ src/app/components/home/AboutMe.tsx
@@ -190,12 +190,7 @@ export const AboutMe = () => {
             <img 
               src="/about-photo.png" 
               alt="Navneet Patidar"
-              className="block dark:hidden w-64 h-auto md:w-72 lg:w-80 object-contain transform hover:scale-[1.03] transition-transform duration-500 ease-out"
-            />
-            <img 
-              src="/about-photo-dark.png" 
-              alt="Navneet Patidar"
-              className="hidden dark:block w-64 h-auto md:w-72 lg:w-80 object-contain transform hover:scale-[1.03] transition-transform duration-500 ease-out"
+              className="w-64 h-auto md:w-72 lg:w-80 object-contain transform hover:scale-[1.03] transition-transform duration-500 ease-out"
             />
           </motion.div>
         </div>
